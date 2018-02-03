08fb862c11faa71600e5ffe090d12239f0e6d989
ricardo perez <rp352429@gmail.com>
2018-02-02 20:17:08 -0400

    animacion velocimetro

--- www/js/controllers/CtrlEstado.js
+++ www/js/controllers/CtrlEstado.js
@@ -1,7 +1,7 @@
 angular.module('app')
-.controller('CtrlEstado',['$scope','$http','localStorageService','Config',CtrlEstado])
+.controller('CtrlEstado',['$scope','$http','$timeout','localStorageService','Config',CtrlEstado])
 
-function CtrlEstado($scope,$http,localStorageService,Config){
+function CtrlEstado($scope,$http,$timeout,localStorageService,Config){
 
 $scope.name = localStorageService.get('seasonName'); 
 $scope.loading = true;
@@ -16,15 +16,19 @@ $scope.loading = true;
 
   }
 
+
 	const config = Config.Headers(localStorageService.get('base64'))
  
 	$http.get("http://globalsens.com:5005/"+localStorageService.get('user')+"/nodes/"+localStorageService.get('root')+"/workspace/humidity", config).then(function (response){
     	$scope.result.date = response.data['last_measures']['Ambient temperature'].timestamp;
-      $scope.number = response.data['saturation'].result;
       $scope.result.sondas = response.data['reference_probes'];
       $scope.result.node_t = response.data['last_measures']['Node temperature'].value;
       $scope.result.floor_t = response.data['last_measures']['Floor temperature'].value;
       $scope.loading = false;
+      $timeout(function() {
+      $scope.number = response.data['saturation'].result;
+      }, 1000);
+
   	},function (){
   		$scope.loading = false;
       $scope.result.bool=true
